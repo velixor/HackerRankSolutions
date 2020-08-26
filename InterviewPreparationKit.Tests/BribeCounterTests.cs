11d80e3bfc350c2626d8142359786e5d28f9e444
Pavel Dushkov <velixor.work@gmail.com>
2020-08-26 16:41:45 +0300

    add BribeCounter

---
+++ InterviewPreparationKit.Tests/BribeCounterTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace InterviewPreparationKit.Tests
+{
+    [TestFixture]
+    public class BribeCounterTests
+    {
+        private BribeCounter _bribeCounter;
+
+        [SetUp]
+        private void SetUp()
+        {
+            _bribeCounter = new BribeCounter(2);
+        }
+
+        [Test]
+        [TestCase(new[] {2, 1, 5, 3, 4}, "3")]
+        [TestCase(new[] {2, 5, 1, 3, 4}, "Too chaotic")]
+        [TestCase(new[] {5, 1, 2, 3, 7, 8, 6, 4}, "Too chaotic")]
+        [TestCase(new[] {1, 2, 5, 3, 7, 8, 6, 4}, "7")]
+        public void CountMinimumBribesForQueue(int[] queue, string answer)
+        {
+            var minBribes = _bribeCounter.CountMinimumBribesForQueue(queue);
+
+            Assert.That(minBribes, Is.EqualTo(answer));
+        }
+    }
+}
---
+++ InterviewPreparationKit/BribeCounter.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+
+// https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+namespace InterviewPreparationKit
+{
+    public class BribeCounter
+    {
+        private const string TooChaotic = "Too chaotic";
+
+        private readonly int _maxBribeCountForPerson;
+
+        private int _bribeCount;
+        private int _pointer;
+        private Person[] _queue;
+
+        public BribeCounter(int maxBribeCountForPerson)
+        {
+            _maxBribeCountForPerson = maxBribeCountForPerson;
+        }
+
+        public string CountMinimumBribesForQueue(int[] q)
+        {
+            Initialize(q);
+
+            return GetMinimumBribeCountOrTooChaotic();
+        }
+
+        private void Initialize(int[] q)
+        {
+            _queue = BuildQueue(q);
+            _bribeCount = 0;
+            _pointer = 0;
+        }
+
+        private static Person[] BuildQueue(int[] q)
+        {
+            return q.Select((x, i) => new Person
+            {
+                PlaceNumber = i + 1,
+                TicketNumber = q[i]
+            }).ToArray();
+        }
+
+        private string GetMinimumBribeCountOrTooChaotic()
+        {
+            if (_queue.Any(x => x.Distance < -_maxBribeCountForPerson)) return TooChaotic;
+
+            while (true)
+            {
+                var queueStatus = UndoBribeInQueue();
+                if (queueStatus == QueueStatus.Sorted) break;
+            }
+
+            return _bribeCount.ToString();
+        }
+
+        private QueueStatus UndoBribeInQueue()
+        {
+            while (_pointer < _queue.Length && _queue[_pointer].Distance <= 0) _pointer++;
+
+            if (_pointer == _queue.Length) return QueueStatus.Sorted;
+
+            var person1 = _queue[_pointer];
+            var person2 = _queue[_pointer - 1];
+
+            SwapPersons(person1, person2);
+
+            _pointer--;
+            _bribeCount++;
+
+            return QueueStatus.Unsorted;
+        }
+
+        private static void SwapPersons(Person person1, Person person2)
+        {
+            var t = person1.TicketNumber;
+            person1.TicketNumber = person2.TicketNumber;
+            person2.TicketNumber = t;
+        }
+
+        private enum QueueStatus
+        {
+            Sorted,
+            Unsorted
+        }
+
+        private class Person
+        {
+            public int TicketNumber { get; set; }
+            public int PlaceNumber { get; set; }
+
+            public int Distance => PlaceNumber - TicketNumber;
+        }
+    }
+}
